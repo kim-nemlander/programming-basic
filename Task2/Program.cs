62cbfba29360952b5d7ff72551f84d60deac5849
kim-nemlander <kim.nemlander@gmail.com>
2017-09-19 14:48:56 +0300

    fix

---
+++ Task2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            int number;
+            Console.WriteLine("Please, enter a number; ");
+            userInput = Console.ReadLine();
+            bool isUserInputTrue = int.TryParse(userInput, out number);
+
+            if (isUserInputTrue == true)
+            {
+                if (number % 2 == 0)
+                {
+                    //Console.WriteLine("The number {0} {1} is negative", number, userInput );
+                    //Console.WriteLine("The number " + number + " is negative", number);
+                    Console.WriteLine($"The number {number} -> {userInput} is an even number");
+                }
+                
+                else
+                {
+                    Console.WriteLine($"The number {number} -> {userInput} is an odd number");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine($"You typed not number {userInput}");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ conditional-statements/Task 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            bool check;
+            do
+            {
+                Console.WriteLine("Please, enter a number:");
+                check = int.TryParse(Console.ReadLine(), out number);
+            }
+            while (!check);
+
+            if (number % 2 == 0)
+                
+                Console.WriteLine($"The number {number}, is an even number.");
+
+            else
+                
+                Console.WriteLine($"The number {number}, is an odd number.");
+                
+                Console.ReadLine();
+        }
+    }
+}
