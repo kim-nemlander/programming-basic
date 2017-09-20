a7a18e6841f1c1b5bd75b3501de412b29a399475
kim-nemlander <kim.nemlander@gmail.com>
2017-09-20 12:56:04 +0300

    task 3 done

---
+++ conditional-statements/Task 3-/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Task_3_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            bool check = true;
+            do
+            {
+                if (check)
+                    Console.WriteLine("Please, enter a number:");
+                else
+                    Console.WriteLine("That's not a number, please enter a number:");
+                check = int.TryParse(Console.ReadLine(), out number);
+
+            }
+            while (!check);
+            {
+                if (number < 0)
+                    Console.Write($"The number {number} is negative ");
+                else if (number > 0)
+                    Console.Write($"The number {number} is positive ");         
+                else
+                    Console.Write($"The number {number} is neutral ");
+
+                if (number % 2 == 0)
+                    Console.WriteLine($"and it is an even number.");
+                else
+                    Console.WriteLine($"and it is an odd number.");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
